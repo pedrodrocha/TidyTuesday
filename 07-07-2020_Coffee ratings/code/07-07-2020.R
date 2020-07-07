474234d42f6b8c5f8a1d78d13a07b0e29f42a97d
Pedro Diniz Rocha <63107472+pedrodrocha@users.noreply.github.com>
2020-07-07 12:33:46 -0300

    Update 07-07-2020.R

--- 07-07-2020_Coffee ratings/code/07-07-2020.R
+++ 07-07-2020_Coffee ratings/code/07-07-2020.R
@@ -2,25 +2,24 @@
 library(tidyverse)
 library(stringr)
 library(ggrepel)
-library(ggthemes)
-library(RColorBrewer)
 library(hrbrthemes)
+
 # Importe o dataset
 tuesdata <- tidytuesdayR::tt_load(2020, week = 28)
 coffe_ratings <- tuesdata$coffee_ratings
 
 
-# Crie uma função para Iteração
+# Crie uma funÃ§Ã£o para IteraÃ§Ã£o
 plot <- function(year){ # Recebe ano de argumento
   
-   # Transformação dos dados
+   # TransformaÃ§Ã£o dos dados
   transform <- coffe_ratings %>% 
     mutate(country_of_origin = ifelse(country_of_origin == "Tanzania, United Republic Of","Tanzania",country_of_origin)) %>%  # Limpando o nome da Tanzania
     mutate(grading_year = as.numeric((str_extract(grading_date, "[0-9]{4}")))) %>%  # Usando regex para criar uma coluna de anos
     filter(species == "Arabica") %>% # Filtrando para o Arabica
     filter(grading_year == year) %>% # O argumento do ano vem aqui 
     select(country_of_origin, grading_year, total_cup_points) %>%
-    group_by(country_of_origin,grading_year)  %>% # agrupando por país e ano
+    group_by(country_of_origin,grading_year)  %>% # agrupando por paÃ­s e ano
     summarise(median = median(total_cup_points)) %>% 
     ungroup() %>%  
     group_by(grading_year) %>%  # agrupando por ano
@@ -36,7 +35,7 @@ plot <- function(year){ # Recebe ano de argumento
                          label = as.factor(country_of_origin)),
                      seed = 123) +
     labs(title = "Which country has the best Arabica Coffee in the World?",
-         subtitle = paste0("According to Coffee Quality Institute's trained reviewers • ", transform$grading_year), # usando paste0() para mudar o subtítulo conforme o ano 
+         subtitle = paste0("According to Coffee Quality Institute's trained reviewers â€¢ ", transform$grading_year), # usando paste0() para mudar o subtÃ­tulo conforme o ano 
          y = "Median grade", x = "",
          caption = "Data: James LeDoux (@jmzledoux)") +
     theme_ipsum(axis_title_just = "center") +
@@ -51,10 +50,10 @@ plot <- function(year){ # Recebe ano de argumento
 }
 
 
-# Dataframe de anos para iteração
+# Dataframe de anos para iteraÃ§Ã£o
 years <- tibble(years = as.character(2010:2018))
 
-# Iteração para criar um plot pra cada ano
+# IteraÃ§Ã£o para criar um plot pra cada ano
 for (year in years$years){
   plot(year)
   ggsave(filename = paste0(year,".png"),
@@ -63,5 +62,5 @@ for (year in years$years){
 
 # Criando o gif
 gifski::gifski(png_files = c("2010.png","2011.png","2012.png","2013.png","2014.png","2015.png","2015.png","2016.png","2017.png","2018.png"),
-               width = 802, height = 323) # PERIGO - o tamanho aqui tem que ser em pixels. Para conversão dos valores: http://auctionrepair.com/pixels.html
+               width = 802, height = 323) # PERIGO - o tamanho aqui tem que ser em pixels. Para conversÃ£o dos valores: http://auctionrepair.com/pixels.html
 
